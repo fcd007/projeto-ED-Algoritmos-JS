0491a8e64cde1370d07146b61e8ec8c92df7075a
Claudeilton Dantas <fcd007@hotmail.com>
2020-08-18 08:04:15 -0300

    Escopo de objetos

---
+++ conceitos-basicos/PraticasJS/OrientacaoObjetos/EstruturaObjeto.js
@@ -0,0 +1,12 @@
+// A estrutura de um objeto em Javascript pode ser definida
+var objeto = new Object();
+
+var objeto2 = {};
+
+var obj = {
+  name: {
+    first: "Francisco",
+    last: "Dantas",
+  },
+  address: "Middle Hilte",
+};
