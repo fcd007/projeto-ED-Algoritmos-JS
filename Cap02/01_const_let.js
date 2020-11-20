3276c5dfab13e4067ba72138997f3b70275f787b
claudeilton dantas <fcd007@hotmail.com>
2020-11-20 08:32:38 -0300

    escopo de variaveis

---
+++ Cap02/01_const_let.js
@@ -0,0 +1,12 @@
+//const  e let no lugar de var
+var framework = 'Angular';
+var framework = 'ReactJS';
+console.log('Framework: ', framework);
+//devemos substituir a palavra eservada const por let
+let language ='Javascript';
+//let language = 'Java'; // lançará um erro porque uma variável language já foi declarada
+console.log('Language: ', language);
+
+const PI = 3.141593;
+//PI = 3.0; //lança um erro - variável apenas para leitura - assinment to constant variable
+console.log('PI: ', PI);
